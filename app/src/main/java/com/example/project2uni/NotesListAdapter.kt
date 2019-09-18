670ca68f167800dd7f580f37a1a7ffb1f6a48cda
root <root@A309-21L.imd.local>
2019-09-18 14:34:29 -0300

    add notes layout

--- app/src/main/java/com/example/project2uni/NotesListAdapter.kt
+++ app/src/main/java/com/example/project2uni/NotesListAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.project2uni
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -8,10 +10,11 @@ import androidx.recyclerview.widget.RecyclerView
 //https://medium.com/collabcode/criando-lista-com-recyclerview-no-android-com-kotlin-85cb76f3775d
 
 
-class NotesListAdapter (private val notes: List <Note>): RecyclerView.Adapter<NotesListAdapter.ViewHolder>() {
+class NotesListAdapter (private val notes: List <Note>, private val context: Context): RecyclerView.Adapter<NotesListAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val noteview = LayoutInflater.from(context).inflate(R.layout.note_item, parent, false)
+        return ViewHolder(noteview)
     }
 
     override fun getItemCount(): Int {
