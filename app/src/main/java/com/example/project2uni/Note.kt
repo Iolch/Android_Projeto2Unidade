4725072da2ced2a29e5aaf6bd7efb6c89a4c9111
root <root@A309-21L.imd.local>
2019-09-18 14:27:51 -0300

    add note interface

---
+++ app/src/main/java/com/example/project2uni/Note.kt
@@ -0,0 +1,3 @@
+package com.example.project2uni
+
+class Note (val title:String, val description:String)
--- app/src/main/java/com/example/project2uni/NotesListAdapter.kt
+++ app/src/main/java/com/example/project2uni/NotesListAdapter.kt
@@ -8,14 +8,14 @@ import androidx.recyclerview.widget.RecyclerView
 //https://medium.com/collabcode/criando-lista-com-recyclerview-no-android-com-kotlin-85cb76f3775d
 
 
-class NotesListAdapter : RecyclerView.Adapter<NotesListAdapter.ViewHolder>() {
+class NotesListAdapter (private val notes: List <Note>): RecyclerView.Adapter<NotesListAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return notes.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
