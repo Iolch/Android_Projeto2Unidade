6cac307aa628405f01eef3d6a3edacc7355f695f
root <root@A309-21L.imd.local>
2019-09-18 14:14:50 -0300

    begin to implement recycler class

---
+++ app/src/main/java/com/example/project2uni/NotesListAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.project2uni
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+//TURORIALLLLL MEDIUMMMM
+//https://medium.com/collabcode/criando-lista-com-recyclerview-no-android-com-kotlin-85cb76f3775d
+
+
+class NotesListAdapter : RecyclerView.Adapter<NotesListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    }
+}
