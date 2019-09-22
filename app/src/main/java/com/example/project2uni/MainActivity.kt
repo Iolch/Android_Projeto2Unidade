f7915b692199f3872207725cd02ae806047c8051
Iolanda <iccpaiva@gmail.com>
2019-09-22 07:16:10 -0700

    feat: finaliza parte de recyclerview

--- app/src/main/java/com/example/project2uni/MainActivity.kt
+++ app/src/main/java/com/example/project2uni/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.project2uni
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = NotesListAdapter(notes(), this)
+
+        recyclerView = findViewById<RecyclerView>(R.id.note_list_recyclerview).apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+    }
+
+    private fun notes(): List<Note> {
+        return listOf(
+            Note("Leitura",
+                "Livro de Kotlin com Android"),
+            Note("Pesquisa",
+                "Como posso melhorar o código dos meus projetos"),
+            Note("Estudo",
+                "Como sincronizar minha App com um Web Service"))
     }
 }
--- app/src/main/java/com/example/project2uni/NotesListAdapter.kt
+++ app/src/main/java/com/example/project2uni/NotesListAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.note_item.view.*
 
 //TURORIALLLLL MEDIUMMMM
 //https://medium.com/collabcode/criando-lista-com-recyclerview-no-android-com-kotlin-85cb76f3775d
@@ -22,9 +23,16 @@ class NotesListAdapter (private val notes: List <Note>, private val context: Con
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val note = notes[position]
+        holder.title.text = note.title
+        holder.description.text = note.description
+
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        val title = itemView.note_item_title
+        val description = itemView.note_item_description
+
     }
 }
