f1defba15530eed2d27d728018de6b6835599d14
Android BTI <android.bti@A309-21L.imd.local>
2019-09-25 14:33:16 -0300

    fix: dialog

--- app/src/main/java/com/example/project2uni/NameNoteDialogFragment.kt
+++ app/src/main/java/com/example/project2uni/NameNoteDialogFragment.kt
@@ -10,18 +10,41 @@ import android.util.Log
 import android.widget.EditText
 import androidx.fragment.app.DialogFragment
 import android.app.Activity
+import androidx.fragment.app.FragmentManager
 
 
+class NameNoteDialogFragment: DialogFragment(){
 
-class NameNoteDialogFragment(context: Context, private val dialogInterface: DialogInterface.OnClickListener): DialogFragment(){
+    private var editText: EditText? = null
+    private var listener: OnTextListener? = null
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return activity?.let {
-            val builder = AlertDialog.Builder(it)
-            val inflater = requireActivity().layoutInflater;
-            builder.setView(inflater.inflate(R.layout.dialog_name_note, null))
-                    .setPositiveButton("Ok", dialogInterface)
-                    .setTitle("Nome da Nota")
-            builder.create()
-        } ?: throw IllegalStateException("Activity cannot be null")
+        val builder = androidx.appcompat.app.AlertDialog.Builder(activity!!)
+        builder.setTitle("Nome da Nota")
+        builder.setPositiveButton("OK") { dialogInterface, i ->
+            if (listener != null) {
+                val text = editText!!.text.toString()
+                listener!!.onSetTExt(text)
+            }
+        }
+
+        builder.setNegativeButton("Cancelar") { dialogInterface, i -> dismiss() }
+
+        val view = activity!!.layoutInflater.inflate(R.layout.activity_edit_note, null)
+        editText = view.findViewById(R.id.note_text)
+        builder.setView(view)
+        return builder.create()
+    }
+    interface OnTextListener {
+        fun onSetTExt(text: String)
+    }
+    companion object {
+        fun show(fm: FragmentManager, listener: OnTextListener) {
+
+            val dialog = NameNoteDialogFragment()
+            dialog.listener = listener
+            dialog.show(fm, "textDialog")
+
+        }
     }
 }
